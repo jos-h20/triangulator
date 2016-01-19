1179b6c9d09d655938849f10aa648cdfb3cc9ea9
Josh Overly and Jon Sweeney <jonsweeney26@gmail.com>
2016-01-19 09:13:59 -0800

    add structure to program

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+
+
+
+
+// $(function() {
+//
+//     $("#result").show();
+//     event.preventDefault();
+//   });
+// });
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('leapYear', function() {
+  it("is scalene for a triangle with unequal sides", function() {
+    expect(traingle(1,2,3)).to.equal("scalene");
+
+});
