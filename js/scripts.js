9d7e80a835364933717d40591bf1739a299be68c
Josh Overly and Jon Sweeney <joshoverly@yahoo.com>
2016-01-19 10:23:41 -0800

    add html to program

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,30 @@
 var triangle = function(side1, side2, side3) {
   if((side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)) { //triangle?
     if(side1 === side2 && side1 === side3) { //equilateral?
-      return "equilateral";
+      return "an equilateral ";
     } else if((side1 === side2 && side1 !== side3) ||(side2 === side3 && side2 !== side1) || (side1 === side3 && side1 !== side2)) { //isosceles?
-      return "isosceles";
+      return "an isosceles ";
     } else {                      //is scalene
-      return "scalene";
+      return "a scalene ";
     }
   } else {
-      return "notTriangle";
+      return "is not a ";
     };
 };
 
 
 
-// $(function() {
-//
-//     $("#result").show();
-//     event.preventDefault();
-//   });
-// });
+$(function() {
+  $("form#isTriangle").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = triangle(side1, side2, side3);
+
+    $(".triangle").text(result);
+
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
