5029c8953f1bd3d72dc8ec2c23dc1ac6675a76b6
Josh Overly and Jon Sweeney <joshoverly@yahoo.com>
2016-01-19 10:01:28 -0800

    run specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-
+var triangle = function(side1, side2, side3) {
+  if((side1 + side2 > side3) && (side2 + side3 > side1) && (side1 + side3 > side2)) { //triangle?
+    if(side1 === side2 && side1 === side3) { //equilateral?
+      return "equilateral";
+    } else if((side1 === side2 && side1 !== side3) ||(side2 === side3 && side2 !== side1) || (side1 === side3 && side1 !== side2)) { //isosceles?
+      return "isosceles";
+    } else {                      //is scalene
+      return "scalene";
+    }
+  } else {
+      return "notTriangle";
+    };
+};
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,18 @@
-describe('leapYear', function() {
+describe('triangle', function() {
   it("is scalene for a triangle with unequal sides", function() {
-    expect(traingle(1,2,3)).to.equal("scalene");
+    expect(triangle(4,2,3)).to.equal("scalene");
+  });
+
+  it("is equilateral for a triangle with all equal sides", function() {
+    expect(triangle(2,2,2)).to.equal("equilateral");
+  });
+
+  it("is isosceles for a triangle with two equal sides", function() {
+    expect(triangle(2,2,3)).to.equal("isosceles");
+  });
+
+  it("is not a triangle for the sum of 2 sides is less or equal to the third side", function() {
+    expect(triangle(1,2,5)).to.equal("notTriangle");
+  });
 
 });
