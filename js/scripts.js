e264be2158a9c26c13327bc4f572e5768745f2cc
Josh Overly and Jon Sweeney <jonsweeney26@gmail.com>
2016-01-19 16:36:03 -0800

    refactor js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,28 @@
 var triangle = function(a, b, c) {
-  if((a + b > c) && (b + c > a) && (a + c > b)) { //triangle?
-    if(a === b && a === c) { //equilateral?
-      return "an equilateral ";
-    } else if((a === b && a !== c) ||(b === c && b !== a) || (a === c && a !== b)) { //isosceles?
-      return "an isosceles ";
-    } else {                      //is scalene
-      return "a scalene ";
+  if (isTriangle(a, b, c)) {
+      return triangleType(a, b, c);
+    } else {
+      return "not a ";
     }
-  } else {
-      return "is not a ";
-    };
-};
+  };
 
+var triangleType = function(a, b, c) {
+  if(a === b && a === c) { //equilateral?
+    return "an equilateral ";
+  } else if((a === b) || (b === c) || (a === c)) { //isosceles?
+    return "an isosceles ";
+  } else {                      //is scalene
+    return "a scalene ";
+  }
+}
+
+var isTriangle = function(a, b, c) {
+  if((a + b > c) && (b + c > a) && (a + c > b)) {//triangle?
+    return true;
+  } else {
+    return false;
+  }
+};
 
 
 $(function() {
@@ -27,17 +38,17 @@ $(function() {
     event.preventDefault();
   });
 });
-
-  $(function() {
-    $("form#isTriangle2").submit(function(event) {
-      var side4 = parseInt($("input#side4").val());
-      var side5 = parseInt($("input#side5").val());
-      var side6 = parseInt($("input#side6").val());
-      var result2 = triangle(side4, side5, side6);
-
-      $(".triangle2").text(result2)
-
-      $("#result2").show();
-      event.preventDefault();
-    });
-});
+//
+//   $(function() {
+//     $("form#isTriangle2").submit(function(event) {
+//       var side4 = parseInt($("input#side4").val());
+//       var side5 = parseInt($("input#side5").val());
+//       var side6 = parseInt($("input#side6").val());
+//       var result2 = triangle(side4, side5, side6);
+//
+//       $(".triangle2").text(result2)
+//
+//       $("#result2").show();
+//       event.preventDefault();
+//     });
+// });
